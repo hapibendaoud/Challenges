88d6323e20e8b643a66660df8550c03f720c22df
hapibendaoud <hapibendaoud@gmail.com>
2026-09-16 18:09:05 +0200

    challenge1

---
+++ challenge1.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+const name = prompt("What is your name?");
+const age = prompt("What is your age?");
+
+console.log(`Hello ${name}, you are ${age} years old.`);
+
+
+
+
+
+
+
