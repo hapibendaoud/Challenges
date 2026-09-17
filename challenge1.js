e6e1e7c95b2341863c306b2123f14d945b574385
hapibendaoud <hapibendaoud@gmail.com>
2026-09-17 11:18:50 +0200

    challenge1 for the day two

--- challenge1.js
+++ challenge1.js
@@ -1,9 +1,18 @@
 const prompt = require('prompt-sync')();
 
-const name = prompt("What is your name?");
-const age = prompt("What is your age?");
+const firstName = prompt("What is your First name? ");
+const lastName = prompt("What is your Last name? ");
+const age = prompt("What is your age? ");
+const sexe = prompt("What is your Sexe? ");
+const email = prompt("What is your Email? ");
 
-console.log(`Hello ${name}, you are ${age} years old.`);
+console.log("This is your informations :");
+console.log(`First name: ${firstName}`);
+console.log(`Last name: ${lastName}`);
+console.log(`Age: ${sexe}`);
+console.log(`Sexe: ${sexe}`);
+console.log(`Email: ${email}`);
+
 
 
 
